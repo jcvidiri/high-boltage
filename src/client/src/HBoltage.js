d41e7ddee8ca0095ae3c23c9f2b1bcf755442be2
Juan Cruz Vidiri <jcvidiri@gmail.com>
2018-09-02 01:57:01 -0300

    Add state and handler (#6)

--- src/client/src/HBoltage.js
+++ src/client/src/HBoltage.js
@@ -3,13 +3,24 @@ import './HBoltage.css'
 import Explorer from './features/Explorer'
 import Navbar from './commons/Navbar'
 import Footer from './commons/Footer'
-
+import CssBaseline from '@material-ui/core/CssBaseline'
 class HBoltage extends Component {
+  state = {
+    showing: 'explorer'
+  }
+
+  handleStateChange = selected => {
+    this.setState({showing: selected})
+  }
+
   render() {
+    const {showing} = this.state
+
     return (
       <div className="HBoltage">
-        <Navbar />
-        <Explorer />
+        <CssBaseline />
+        <Navbar handleStateChange={this.handleStateChange} showing={this.state.showing} />
+        {showing === 'explorer' && <Explorer />}
         <Footer />
       </div>
     )
--- src/client/src/commons/Navbar/components/Navbar.js
+++ src/client/src/commons/Navbar/components/Navbar.js
@@ -5,7 +5,8 @@ import Typography from '@material-ui/core/Typography'
 import Button from '@material-ui/core/Button'
 
 function Navbar(props) {
-  const {classes} = props
+  const {classes, showing, handleStateChange} = props
+
   return (
     <div className={classes.root}>
       <AppBar position="static">
@@ -13,9 +14,33 @@ function Navbar(props) {
           <Typography variant="title" color="inherit" className={classes.flex}>
             High Boltage Explorer
           </Typography>
-          <Button color="inherit">Blocks</Button>
-          <Button color="inherit">Minters & Peers</Button>
-          <Button color="inherit">Add transaction</Button>
+          <Button
+            variant={showing === 'explorer' ? 'outlined' : null}
+            color="inherit"
+            onClick={() => {
+              handleStateChange('explorer')
+            }}
+          >
+            Explorer
+          </Button>
+          <Button
+            variant={showing === 'minters' ? 'outlined' : null}
+            color="inherit"
+            onClick={() => {
+              handleStateChange('minters')
+            }}
+          >
+            Minters & Peers
+          </Button>
+          <Button
+            variant={showing === 'add' ? 'outlined' : null}
+            color="inherit"
+            onClick={() => {
+              handleStateChange('add')
+            }}
+          >
+            Add transaction
+          </Button>
         </Toolbar>
       </AppBar>
     </div>
