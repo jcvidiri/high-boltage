639becc0c2e4d9d6bd4f3e8bdf8869241b892e65
Juan Cruz Vidiri <jcvidiri@gmail.com>
2018-09-02 18:16:44 -0300

    new fodler structure

--- server/src/index.js
+++ server/src/index.js
@@ -10,9 +10,9 @@ dotenv.config()
 const HTTP_PORT = process.env.HTTP_PORT || 3001
 const IP_ADDRESS = process.env.IP_ADDRESS
 const P2P_PORT = process.env.P2P_PORT || 6001
-const INITIAL_PEERS = process.env.PEERS ? process.env.PEERS.split(',') : [];
+const INITIAL_PEERS = process.env.PEERS ? process.env.PEERS.split(',') : []
 
-//EXPRESS
+// EXPRESS
 const app = express()
 
 app.use(routes)
@@ -23,6 +23,6 @@ app.listen(HTTP_PORT, IP_ADDRESS, function () {
   console.log('Server listening at', this.address())
 })
 
-//WEBSOCKET
+// WEBSOCKET
 webSocket.initP2PServer(P2P_PORT, INITIAL_PEERS)
 webSocket.connectToPeers(INITIAL_PEERS)
